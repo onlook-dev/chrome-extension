6becc748c3cf75f84ff8ad67fb425a6b379332e6
Kiet Ho <hoakiet98@gmail.com>
2024-01-09 10:47:21 -0500

    Configured Firebase

--- dashboard.sveltekit/src/lib/utils/env.ts
+++ dashboard.sveltekit/src/lib/utils/env.ts
@@ -1,40 +1,39 @@
 import {
-	TEST_API_KEY,
-	TEST_AUTH_DOMAIN,
-	TEST_PROJECT_ID,
-	TEST_STORAGE_BUCKET,
-	TEST_MESSAGE_SENDER_ID,
-	TEST_APP_ID,
-	TEST_MEASUREMENT_ID,
-	PROD_API_KEY,
-	PROD_AUTH_DOMAIN,
-	PROD_PROJECT_ID,
-	PROD_STORAGE_BUCKET,
-	PROD_MESSAGE_SENDER_ID,
-	PROD_APP_ID,
-	PROD_MEASUREMENT_ID,
-	NODE_ENV
-} from '$env/static/private';
+	PUBLIC_TEST_API_KEY,
+	PUBLIC_TEST_AUTH_DOMAIN,
+	PUBLIC_TEST_PROJECT_ID,
+	PUBLIC_TEST_STORAGE_BUCKET,
+	PUBLIC_TEST_MESSAGE_SENDER_ID,
+	PUBLIC_TEST_APP_ID,
+	PUBLIC_TEST_MEASUREMENT_ID,
+	PUBLIC_PROD_API_KEY,
+	PUBLIC_PROD_AUTH_DOMAIN,
+	PUBLIC_PROD_PROJECT_ID,
+	PUBLIC_PROD_STORAGE_BUCKET,
+	PUBLIC_PROD_MESSAGE_SENDER_ID,
+	PUBLIC_PROD_APP_ID,
+	PUBLIC_PROD_MEASUREMENT_ID
+} from '$env/static/public';
 
 const testFirebaseConfig = {
-	apiKey: TEST_API_KEY,
-	authDomain: TEST_AUTH_DOMAIN,
-	projectId: TEST_PROJECT_ID,
-	storageBucket: TEST_STORAGE_BUCKET,
-	messagingSenderId: TEST_MESSAGE_SENDER_ID,
-	appId: TEST_APP_ID,
-	measurementId: TEST_MEASUREMENT_ID
+	apiKey: PUBLIC_TEST_API_KEY,
+	authDomain: PUBLIC_TEST_AUTH_DOMAIN,
+	projectId: PUBLIC_TEST_PROJECT_ID,
+	storageBucket: PUBLIC_TEST_STORAGE_BUCKET,
+	messagingSenderId: PUBLIC_TEST_MESSAGE_SENDER_ID,
+	appId: PUBLIC_TEST_APP_ID,
+	measurementId: PUBLIC_TEST_MEASUREMENT_ID
 };
 
 const prodFirebaseConfig = {
-	apiKey: PROD_API_KEY,
-	authDomain: PROD_AUTH_DOMAIN,
-	projectId: PROD_PROJECT_ID,
-	storageBucket: PROD_STORAGE_BUCKET,
-	messagingSenderId: PROD_MESSAGE_SENDER_ID,
-	appId: PROD_APP_ID,
-	measurementId: PROD_MEASUREMENT_ID
+	apiKey: PUBLIC_PROD_API_KEY,
+	authDomain: PUBLIC_PROD_AUTH_DOMAIN,
+	projectId: PUBLIC_PROD_PROJECT_ID,
+	storageBucket: PUBLIC_PROD_STORAGE_BUCKET,
+	messagingSenderId: PUBLIC_PROD_MESSAGE_SENDER_ID,
+	appId: PUBLIC_PROD_APP_ID,
+	measurementId: PUBLIC_PROD_MEASUREMENT_ID
 };
 
-export const isDevelopment = NODE_ENV === 'development';
+export const isDevelopment = process.env.NODE_ENV === 'development';
 export const firebaseConfig = isDevelopment ? testFirebaseConfig : prodFirebaseConfig;
