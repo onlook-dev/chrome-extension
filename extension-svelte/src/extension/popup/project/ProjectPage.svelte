8b2f01dfbfe72865ee483553fe9bdf601d452637
Kiet Ho <hoakiet98@gmail.com>
2024-01-12 17:05:14 -0500

    Creating and viewing projects

--- extension-svelte/src/extension/popup/project/ProjectPage.svelte
+++ extension-svelte/src/extension/popup/project/ProjectPage.svelte
@@ -4,9 +4,10 @@
 	import { projectsMapBucket, popupStateBucket } from '$lib/utils/localstorage'
 	import { PopupRoutes } from '$lib/utils/constants'
 	import ArrowLeft from '~icons/formkit/arrowleft'
+	import Pencil from '~icons/mdi/pencil'
 
-	let project: Project
-
+	let project: Project | undefined
+	const tabsName = 'project-tabs-id'
 	function returnToDashboard() {
 		popupStateBucket.set({ activeRoute: PopupRoutes.DASHBOARD })
 	}
@@ -19,16 +20,48 @@
 	})
 </script>
 
-<div class="navbar p-none">
-	<div class="flex-none">
+<div class="navbar p-none border">
+	<div class="flex-1">
 		<button on:click={returnToDashboard} class="btn btn-sm btn-ghost">
 			<ArrowLeft /> Dashboard</button
 		>
 	</div>
+	<div class="flex-none">
+		<button class="btn btn-sm btn-outline">
+			<Pencil />
+			{#if project?.changeSets.length}
+				Edit
+			{:else}
+				Start editing
+			{/if}
+		</button>
+		{#if project?.changeSets.length}
+			<button class="ml-2 btn btn-sm btn-primary">Publish</button>
+		{/if}
+	</div>
 </div>
 
 {#if project}
-	<div></div>
+	<div role="tablist" class="tabs tabs-bordered tabs-md bt py-4">
+		<input type="radio" name={tabsName} role="tab" class="tab" aria-label="General" checked />
+		<div role="tabpanel" class="tab-content">
+			<div class="flex flex-col gap-4 w-52">
+				<div class="skeleton h-32 w-full"></div>
+				<div class="skeleton h-4 w-28"></div>
+				<div class="skeleton h-4 w-full"></div>
+				<div class="skeleton h-4 w-full"></div>
+			</div>
+		</div>
+
+		<input type="radio" name={tabsName} role="tab" class="tab" aria-label="Comments" />
+		<div role="tabpanel" class="tab-content">Tab content 2</div>
+
+		<input type="radio" name={tabsName} role="tab" class="tab" aria-label="Activities" />
+		<div role="tabpanel" class="tab-content">Tab content 3</div>
+
+		<input type="radio" name={tabsName} role="tab" class="tab" aria-label="Settings" />
+		<div role="tabpanel" class="tab-content">Tab content 3</div>
+	</div>
 {:else}
 	<div class="flex flex-col gap-4 w-52">
 		<div class="skeleton h-32 w-full"></div>
