26f47a43f015c20e8a907c50da183f2c26b70735
eabnelson <eabnelson@gmail.com>
2024-01-24 10:26:29 -0500

    unused imports

--- dashboard-sveltekit/src/lib/storage/project.ts
+++ dashboard-sveltekit/src/lib/storage/project.ts
@@ -1,9 +1,6 @@
 import { getObjectFromCollection, subscribeToDocument } from '$lib/firebase/firestore';
-import { createBranch, createPRWithComments } from '$lib/github/github';
 import { FIREBASE_COLLECTION_PROJECTS } from '$shared/constants';
 import type { Project } from '$shared/models/project';
-import { getUserFromFirebase } from './user';
-import { Octokit } from '@octokit/core';
 
 export async function getProjectFromFirebase(projectId: string): Promise<Project> {
 	const projectData = await getObjectFromCollection(FIREBASE_COLLECTION_PROJECTS, projectId);
