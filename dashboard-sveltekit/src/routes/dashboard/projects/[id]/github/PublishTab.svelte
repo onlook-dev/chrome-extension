4161afef4d64d29e563939006aa3d96994ed42f4
Kiet Ho <hoakiet98@gmail.com>
2024-01-25 17:39:32 -0500

    Checking for path

--- dashboard-sveltekit/src/routes/dashboard/projects/[id]/github/PublishTab.svelte
+++ dashboard-sveltekit/src/routes/dashboard/projects/[id]/github/PublishTab.svelte
@@ -4,14 +4,28 @@
 	import { exportToPRComments } from '$lib/github/github';
 	import { DashboardRoutes } from '$shared/constants';
 	import type { Project } from '$shared/models/project';
+
 	import SvelteCodeblock from './SvelteCodeblock.svelte';
 	import NextjsCodeblock from './NextjsCodeblock.svelte';
+	import GitHub from '~icons/mdi/github';
 
 	export let project: Project;
 	export let userId: string;
 
 	let isLoading = false;
 	let prLink: string | undefined;
+	let pathFound = false;
+
+	onMount(() => {
+		// Check each activities for a path
+		Object.values(project.activities).forEach((activity) => {
+			if (activity.path) {
+				// If a path is found, open the modal
+				pathFound = true;
+				return;
+			}
+		});
+	});
 
 	async function handlePublishClick() {
 		if (!project?.githubSettings) {
@@ -33,50 +47,58 @@
 </script>
 
 <div class="flex flex-col items-center justify-center h-full mt-4">
-	<p class="text-md text-center">
-		<b>{new URL(project.hostUrl).host}</b> is not configured with Onlook. <br />Follow instructions
-		below.
-	</p>
+	{#if pathFound}
+		{#if isLoading}
+			<button disabled class="btn btn-outline"> Loading... </button>
+		{:else if prLink}
+			<a href={prLink} target="_blank" class="btn btn-outline">
+				<GitHub class="w-5 h-5" />
+				View changes in Github
+			</a>
+		{:else}
+			<button class="btn btn-outline" on:click={handlePublishClick}>
+				<GitHub class="w-5 h-5" />
+				Publish changes to Github
+			</button>
+		{/if}
+	{:else}
+		<p class="text-md text-center">
+			<b>{new URL(project.hostUrl).host}</b> is not configured with Onlook. <br />Follow
+			instructions below.
+		</p>
 
-	<div class="text-start max-w-[100%] space-y-4 my-4">
-		<div>
-			<p class="mt-3 font-semibold">1. Install the onlook library:</p>
-			<div role="tablist" class="tabs tabs-bordered">
-				<input type="radio" name="my_tabs_2" role="tab" class="tab" aria-label="Next.js" checked />
-				<div role="tabpanel" class="tab-content bg-base-100 rounded-box py-4 overflow-auto">
-					<NextjsCodeblock />
+		<div class="text-start max-w-[100%] space-y-4 my-4">
+			<div>
+				<p class="mt-3 font-semibold">1. Install the onlook library:</p>
+				<div role="tablist" class="tabs tabs-bordered">
+					<input
+						type="radio"
+						name="my_tabs_2"
+						role="tab"
+						class="tab"
+						aria-label="Next.js"
+						checked
+					/>
+					<div role="tabpanel" class="tab-content bg-base-100 rounded-box py-4 overflow-auto">
+						<NextjsCodeblock />
+					</div>
+					<input type="radio" name="my_tabs_2" role="tab" class="tab" aria-label="Svelte" />
+					<div role="tabpanel" class="tab-content bg-base-100 rounded-box py-4 overflow-auto">
+						<SvelteCodeblock />
+					</div>
 				</div>
-				<input type="radio" name="my_tabs_2" role="tab" class="tab" aria-label="Svelte" />
-				<div role="tabpanel" class="tab-content bg-base-100 rounded-box py-4 overflow-auto">
-					<SvelteCodeblock />
-				</div>
+				<p class="">This will add a secure onlook identifier in your app.</p>
 			</div>
-			<p class="">This will add a secure onlook identifier in your app.</p>
-		</div>
 
-		<div>
-			<p class="font-semibold py-2">2. Publish project:</p>
-			<p class="">Run your project in localhost or publish it again.</p>
+			<div>
+				<p class="font-semibold py-2">2. Publish project:</p>
+				<p class="">Run your project in localhost or publish it again.</p>
+			</div>
+			<div>
+				<p class="font-semibold py-2">3. Sync your project:</p>
+				<p class="">Open project in Chrome extension and press sync.</p>
+				<p class="">Example.jpg</p>
+			</div>
 		</div>
-		<div>
-			<p class="font-semibold py-2">3. Sync your project:</p>
-			<p class="">Open project in Chrome extension and press sync.</p>
-			<p class="">Example.jpg</p>
-		</div>
-	</div>
+	{/if}
 </div>
-
-<!-- 
-{#if isLoading}
-	<button disabled class="btn btn-outline"> Loading... </button>
-{:else if prLink}
-	<a href={prLink} target="_blank" class="btn btn-outline">
-		<GitHub class="w-5 h-5" />
-		View changes in Github
-	</a>
-{:else}
-	<button class="btn btn-outline" on:click={handlePublishClick}>
-		<GitHub class="w-5 h-5" />
-		Publish changes to Github
-	</button>
-{/if} -->
--- shared/models/activity.ts
+++ shared/models/activity.ts
@@ -5,7 +5,7 @@ export interface Activity {
   id: string;
   userId: string;
   selector: string;
-  path: string; // '<encodedPath>' -> path/to/file:line
+  path?: string; // '<encodedPath>' -> path/to/file:line
   projectId: string;
   eventData: EventMetadata[];
   visible: boolean;
