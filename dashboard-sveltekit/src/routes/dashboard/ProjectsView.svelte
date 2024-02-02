d1be8b29bde27758cc2881911ab1d4d5c239c1c2
Onlook <erik@onlook.dev>
2024-02-02 18:18:25 +0000

    Adding onlook comments to multiple files

--- dashboard-sveltekit/src/routes/dashboard/ProjectsView.svelte
+++ dashboard-sveltekit/src/routes/dashboard/ProjectsView.svelte
@@ -30,6 +30,7 @@
 <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4">
 	{#if team?.projectIds.length}
 		{#each team?.projectIds.map((id) => $projectsMapStore.get(id)) as project}
+// onlook edits
 			<button
 				on:click={() => goto(`${DashboardRoutes.PROJECTS}/${project?.id}`)}
 				class="bg-base-100 rounded space-y-4 p-4 hover:shadow block"
