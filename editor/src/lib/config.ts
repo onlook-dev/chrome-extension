6034a9c6b96c0ba34267ae72f1482efa11d7f32d
Kiet Ho <hoakiet98@gmail.com>
2024-02-22 21:45:48 -0500

    Update editor for server connection

--- editor/src/lib/config.ts
+++ editor/src/lib/config.ts
@@ -1,3 +1,2 @@
-const SERVER_HTTP_URL = new URL(process.env.NGROK_SERVER_URL || process.env.SERVER_URL || 'http://localhost:4444');
-const SERVER_PROTOCOL = SERVER_HTTP_URL.protocol === 'https:' ? 'wss' : 'ws';
-export const SERVER_SOCKET_URL = `${SERVER_PROTOCOL}://${SERVER_HTTP_URL.host}`;
+const wsProtocol = window.location.protocol === 'https:' ? 'wss:' : 'ws:';
+export const SERVER_SOCKET_URL = `${wsProtocol}://${window.location.host}`; // Use the same host and protocol as the client
