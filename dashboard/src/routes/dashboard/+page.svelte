6fc63b5c2bc2d0a7e50cc893e2638fa0aa371816
Onlook <erik@onlook.dev>
2024-02-28 23:16:07 +0000

    Adding onlook comments to multiple files

--- dashboard/src/routes/dashboard/+page.svelte
+++ dashboard/src/routes/dashboard/+page.svelte
@@ -72,7 +72,7 @@
 			>
 
 			<!-- TODO: Change based on folder -->
-			<h1 class="text-red-500 font-light">
+			<h1 class="text-2xl text-white bg-red-500 font-thin">
 				{$teamsMapStore.get(activeTeamId)?.name ?? 'Unknown team'}
 			</h1>
 		</div>
