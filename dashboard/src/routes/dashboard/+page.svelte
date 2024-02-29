5895c8c3356f15082870c524552e29a382687efa
Onlook <erik@onlook.dev>
2024-02-29 14:41:49 +0000

    Adding onlook comments to multiple files

--- dashboard/src/routes/dashboard/+page.svelte
+++ dashboard/src/routes/dashboard/+page.svelte
@@ -64,7 +64,7 @@
 <div class="drawer lg:drawer-open">
 	<input id={dashboardDrawerId} type="checkbox" class="drawer-toggle" />
 	<!-- Drawer content -->
-	<div class="bg-[#e6e6e6] drawer-content px-4 py-6 overflow-auto h-screen">
+	<div class="bg-[#8a230a] drawer-content px-4 py-6 overflow-auto h-screen">
 		<!-- Page content here -->
 		<div class="flex flex-row gap-2 mb-4 items-center">
 			<label for={dashboardDrawerId} class="btn btn-square btn-ghost drawer-button lg:hidden"
@@ -72,7 +72,7 @@
 			>
 
 			<!-- TODO: Change based on folder -->
-			<h1 class="text-2xl font-bold">
+			<h1 class="text-red-600 text-2xl font-bold">
 				{$teamsMapStore.get(activeTeamId)?.name ?? 'Unknown team'}
 			</h1>
 		</div>
