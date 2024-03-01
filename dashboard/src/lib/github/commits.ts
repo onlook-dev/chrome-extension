f22cc5562b1ebc6af8aa437fa3934ed3a7f63a21
Kiet Ho <hoakiet98@gmail.com>
2024-03-01 09:21:16 -0500

    Cleanup

--- dashboard/src/lib/github/commits.ts
+++ dashboard/src/lib/github/commits.ts
@@ -1,44 +1,9 @@
 import type { Activity } from "$shared/models/activity";
 import type { Octokit } from "@octokit/core";
-import { fetchFileFromPath, getPathInfo, type FileContentData, type PathInfo } from "./files";
+import { fetchFileFromPath, getPathInfo, type FileContentData, } from "./files";
 import type { TreeItem } from "$shared/models/github";
-
-import { activityToTranslationInput, getContentClass, updateContentClass } from '$shared/translation';
 import type { TranslationInput, TranslationOutput } from "$shared/models/translation";
-
-async function getTranslationsFromServer(inputs: TranslationInput[]): Promise<TranslationOutput[]> {
-  const messages = { role: 'user', content: `json: ${JSON.stringify({ "changes": inputs })}` };
-  const response = await fetch('/api/chat', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ messages })
-  });
-
-  if (!response.ok) {
-    throw new Error('Network response was not ok');
-  }
-  const data = await response.json();
-  console.log('data', data);
-  return JSON.parse(data.choices[0].message.content).changes;
-}
-
-function getTranslationInput(content: string, pathInfo: PathInfo, activity: Activity): TranslationInput {
-  const newContent = content.split('\n').slice(pathInfo.startLine - 1, pathInfo.endLine).join('\n');
-  const currentClasses = getContentClass(newContent);
-  const translationInput = activityToTranslationInput(activity, pathInfo, currentClasses,);
-  return translationInput;
-}
-
-function updateContentChunk(content: string, pathInfo: PathInfo, newClass: string): string {
-  let newContent = content.split('\n').slice(pathInfo.startLine - 1, pathInfo.endLine).join('\n');
-  newContent = updateContentClass(newContent, newClass);
-  // Merge new content back into content
-  const contentLines = content.split('\n');
-  contentLines.splice(pathInfo.startLine - 1, pathInfo.endLine - pathInfo.startLine + 1, newContent);
-  return contentLines.join('\n');
-}
+import { getTranslationInput, getTranslationsFromServer, updateContentChunk } from "$lib/translation/translation";
 
 export async function prepareCommit(
   octokit: Octokit,
@@ -60,8 +25,12 @@ export async function prepareCommit(
     }
     const pathInfo = getPathInfo(activity.path, rootPath);
     if (!fileDataMap.has(pathInfo.path)) {
-      const fetchPromise = fetchFileFromPath(octokit, owner, repo, branch, pathInfo.path).then((fileContentData: FileContentData) => {
-        fileDataMap.set(pathInfo.path, fileContentData);
+      const fetchPromise = fetchFileFromPath(octokit, owner, repo, branch, pathInfo.path).then((fileContentData: FileContentData | undefined) => {
+        if (!fileContentData) {
+          console.error("Dile content not found for path: ", pathInfo.path)
+        } else {
+          fileDataMap.set(pathInfo.path, fileContentData);
+        }
       })
       fetchPromises.push(fetchPromise);
     }
