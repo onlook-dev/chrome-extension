58b7e7f0f48d9ad332b497a7503a08f8eff40718
eabnelson <eabnelson@gmail.com>
2024-03-12 15:08:46 -0400

    update max duration

--- dashboard/svelte.config.js
+++ dashboard/svelte.config.js
@@ -1,6 +1,6 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { onlookPreprocess } from '@onlook/svelte';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
