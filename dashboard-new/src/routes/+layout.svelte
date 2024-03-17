a60f201af3abff5611eebaf89f51a17ce0062c7e
Kiet Ho <hoakiet98@gmail.com>
2024-03-17 13:41:42 -0400

    Set up new dashboard

---
+++ dashboard-new/src/routes/+layout.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import '../app.pcss';
+	import { onMount } from 'svelte';
+	import { subscribeToFirebaseAuthChanges } from '$lib/firebase/auth';
+	import { SvelteToast } from '@zerodevx/svelte-toast';
+
+	onMount(() => {
+		subscribeToFirebaseAuthChanges();
+	});
+</script>
+
+<slot />
+<SvelteToast options={{ reversed: true, intro: { y: 192 }, duration: 3000 }} />
+
+<style>
+	:root {
+		--toastBackground: #000000;
+		--toastColor: #ffffff;
+		--toastBarBackground: #ffffff;
+		--toastBorderRadius: 5px;
+		--toastContainerTop: auto;
+		--toastContainerRight: auto;
+		--toastContainerBottom: 2rem;
+		--toastContainerLeft: calc(50vw - 8rem);
+		--toastBarHeight: 0px;
+	}
+</style>
