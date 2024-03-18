4b301ff7e1249788d3cd41adcccff9ee3621529b
Kiet Ho <hoakiet98@gmail.com>
2024-03-18 10:50:10 -0400

    Fix portfolio initialize bug

--- extension/src/lib/editor/inject.js
+++ extension/src/lib/editor/inject.js
@@ -1,15 +1,16 @@
 function addCustomElements() {
+  const ONLOOK_TOOLBAR_TAG = 'onlook-toolbar';
   // @ts-ignore - Browser exists in context 
   var platform = typeof browser === 'undefined' ? chrome : browser;
 
   // Check if the elements are already added to prevent duplicates
-  if (!document.querySelector('onlook-toolbar')) {
+  if (!document.querySelector(ONLOOK_TOOLBAR_TAG)) {
     const onlookScript = document.createElement('script');
     onlookScript.type = 'module';
     onlookScript.src = platform.runtime.getURL('src/lib/editor/bundle.min.js');
     document.body.appendChild(onlookScript);
 
-    const onlookToolbar = document.createElement('onlook-toolbar');
+    const onlookToolbar = document.createElement(ONLOOK_TOOLBAR_TAG);
     document.body.append(onlookToolbar);
   }
 }
@@ -18,16 +19,14 @@ function addCustomElements() {
 const observer = new MutationObserver(function (mutations) {
   mutations.forEach(function (mutation) {
     if (mutation.addedNodes.length || mutation.removedNodes.length) {
+      // Rerun adding custom element if children are mutated
       addCustomElements();
     }
   });
 });
 
-// Specify what to observe
-const config = { childList: true, subtree: true };
-
 // Start observing the body for changes in children
-observer.observe(document.body, config);
+observer.observe(document.body, { childList: true });
 
 // Also run the function initially in case the DOM is already in the desired state
 addCustomElements();
