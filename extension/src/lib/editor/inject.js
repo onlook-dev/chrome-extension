3dc43f8f4874ab9d98f3ba9dd55f9fdf9af2c76d
Kiet Ho <hoakiet98@gmail.com>
2024-03-18 10:43:10 -0400

    Fix portfolio initialize bug

--- extension/src/lib/editor/inject.js
+++ extension/src/lib/editor/inject.js
@@ -1,10 +1,33 @@
-// @ts-ignore - Browser exists in context 
-var platform = typeof browser === 'undefined' ? chrome : browser
+function addCustomElements() {
+  // @ts-ignore - Browser exists in context 
+  var platform = typeof browser === 'undefined' ? chrome : browser;
 
-const onlookScript = document.createElement('script')
-onlookScript.type = 'module'
-onlookScript.src = platform.runtime.getURL('src/lib/editor/bundle.min.js')
-document.body.appendChild(onlookScript)
+  // Check if the elements are already added to prevent duplicates
+  if (!document.querySelector('onlook-toolbar')) {
+    const onlookScript = document.createElement('script');
+    onlookScript.type = 'module';
+    onlookScript.src = platform.runtime.getURL('src/lib/editor/bundle.min.js');
+    document.body.appendChild(onlookScript);
 
-const onlookToolbar = document.createElement('onlook-toolbar')
-document.body.append(onlookToolbar)
+    const onlookToolbar = document.createElement('onlook-toolbar');
+    document.body.append(onlookToolbar);
+  }
+}
+
+// Initialize MutationObserver
+const observer = new MutationObserver(function (mutations) {
+  mutations.forEach(function (mutation) {
+    if (mutation.addedNodes.length || mutation.removedNodes.length) {
+      addCustomElements();
+    }
+  });
+});
+
+// Specify what to observe
+const config = { childList: true, subtree: true };
+
+// Start observing the body for changes in children
+observer.observe(document.body, config);
+
+// Also run the function initially in case the DOM is already in the desired state
+addCustomElements();
