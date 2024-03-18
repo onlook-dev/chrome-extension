c1d233c6ef27b0fe48e5fda6555651f4f23ec55b
Kiet Ho <hoakiet98@gmail.com>
2024-03-18 17:26:37 -0400

    Refactor Github modal

--- dashboard/src/routes/dashboard/projects/[id]/github/PublishTab.svelte
+++ dashboard/src/routes/dashboard/projects/[id]/github/PublishTab.svelte
@@ -17,10 +17,9 @@
 	import type { GithubHistory } from '$shared/models/github';
 	import type { User } from '$shared/models/user';
 
-	import PullRequest from '~icons/ph/git-pull-request-bold';
 	import GitHub from '~icons/mdi/github';
-	import Restore from '~icons/ic/baseline-restore';
 	import ConfigureProjectInstructions from './instructions/ConfigureProjectInstructions.svelte';
+	import GithubHistories from './GithubHistories.svelte';
 
 	export let project: Project;
 	export let user: User;
@@ -38,7 +37,6 @@
 	let isLoading = false;
 	let publishError = false;
 	let publishErrorMessage = '';
-	let historyOpen = true;
 	let errorMessage = '';
 	let prLink: string | undefined;
 
@@ -200,60 +198,7 @@
 			</div>
 		</label>
 
-		{#if githubHistories.length > 0}
-			<div
-				class="collapse collapse-arrow border rounded-md mt-6 {historyOpen
-					? 'collapse-open'
-					: 'collapse-close'}"
-			>
-				<input type="checkbox" on:click={() => (historyOpen = !historyOpen)} />
-				<div class="collapse-title">Publish history ({githubHistories.length})</div>
-				<div class="collapse-content space-y-2">
-					{#each githubHistories as history}
-						<div class="flex flex-row max-w-[100%] items-center">
-							<p class="line-clamp-1 text-ellipsis max-w-[70%]">
-								{history.title}
-							</p>
-							<div class="ml-auto">
-								<div class="tooltip tooltip-left" data-tip="View pull request">
-									<button
-										class="btn btn-xs btn-square btn-ghost ml-auto"
-										on:click={() => window.open(history.pullRequestUrl, '_blank')}
-										><PullRequest class="w-4 h-4" /></button
-									>
-								</div>
-								<div class="tooltip tooltip-left" data-tip="Restore changes">
-									<button
-										class="btn btn-xs btn-square btn-ghost"
-										on:click={() => {
-											// @ts-ignore
-											document.getElementById('confirm_restore_modal').showModal();
-										}}><Restore class="w-4 h-4" /></button
-									>
-								</div>
-
-								<dialog id="confirm_restore_modal" class="modal">
-									<div class="modal-box">
-										<h3 class="font-bold text-lg">Restore changes?</h3>
-										<p class="py-4">This will overwrite your current activities.</p>
-										<div class="modal-action">
-											<form method="dialog">
-												<!-- if there is a button in form, it will close the modal -->
-												<button class="btn">Cancel</button>
-												<button
-													class="btn btn-error ml-2"
-													on:click={() => restoreActivities(history)}>Restore</button
-												>
-											</form>
-										</div>
-									</div>
-								</dialog>
-							</div>
-						</div>
-					{/each}
-				</div>
-			</div>
-		{/if}
+		<GithubHistories {githubHistories} {restoreActivities} />
 	{:else if !githubConfigured}
 		<p class="text-center text-lg">No github config found</p>
 	{:else if hasActivities && !hasFilePaths}
