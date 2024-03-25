3f80d578def9d194f600da5093daecafb4874e5f
Kiet Ho <hoakiet98@gmail.com>
2024-03-25 14:39:27 -0400

    Testing translation integration

--- dashboard/tests/translation/publish.test.ts
+++ dashboard/tests/translation/publish.test.ts
@@ -1,5 +1,5 @@
-// @ts-ignore - Bun test exists
-import { expect, test, describe, mock, beforeAll, fn } from 'bun:test';
+import { expect, test, describe, mock, beforeAll, mockImplementation } from 'bun:test';
+import type { ProjectPublisher } from '$lib/publish';
 
 describe('ProjectPublisher', () => {
   let ProjectPublisher: any;
@@ -8,7 +8,6 @@ describe('ProjectPublisher', () => {
   const mockProject = {
     id: 'mockProjectId',
     name: 'mockProjectName',
-
     installationId: 'mockInstallationId',
     githubSettings: {
       owner: 'mockOwner',
@@ -19,6 +18,33 @@ describe('ProjectPublisher', () => {
     activities: {},
   } as any;
 
+  const mockActivity = {
+    id: 'mockActivityId',
+    styleChanges: {
+      color: { key: 'color', newVal: 'red', oldVal: 'black' },
+      backgroundColor: { key: 'background-color', newVal: 'blue', oldVal: 'white' }
+    },
+    textChanges: {
+      text: { newVal: 'newText', oldVal: 'oldText' },
+    },
+  }
+
+  const mockProcessedActivity = {
+    activity: mockActivity,
+    pathInfo: { path: 'mockPath', startLine: 1, startTagEndLine: 2, endLine: 3 },
+  } as any
+
+  const mockContent = {
+    path: 'mockPath',
+    sha: 'mockSha',
+    content: `<div>
+  <p 
+    class='bg-red'
+  >
+    Old Text
+  </p>`,
+  }
+
   beforeAll(async () => {
     mock.module("$lib/utils/env", () => ({
       openAiConfig: {
@@ -28,17 +54,33 @@ describe('ProjectPublisher', () => {
     }))
 
     mock.module("$lib/translation", () => ({
-      TranslationService: (() => ({
-        getStyleTranslation: () => Promise.resolve('mockStyleTranslation'),
-        getTextTranslation: () => Promise.resolve('mockStyleTranslation'),
-      }))
+      TranslationService: class {
+        async getStyleTranslation(input: any) { return input.code }
+        async getTextTranslation(input: any) { return input.code }
+      }
     }));
+
+    mock.module("$lib/github", () => {
+      const GithubService = class { };
+      return { GithubService };
+
+    });
+
     const Publisher = await import('$lib/publish');
     ProjectPublisher = Publisher.ProjectPublisher;
   });
 
-  test('should throw error if project has no installation ID', () => {
-    const project = { ...mockProject, installationId: undefined };
+  test('should throw error if installationId is missing', () => {
+    let project = { ...mockProject, installationId: undefined };
     expect(() => new ProjectPublisher(project, mockUser)).toThrow();
+
+    project = { ...project, installationId: {} };
+    expect(() => new ProjectPublisher(project, mockUser)).not.toThrow();
+  });
+
+  test('should get correct style change', async () => {
+    const publisher: ProjectPublisher = new ProjectPublisher(mockProject, mockUser);
+    const { content } = await publisher.updateFileWithActivity(mockProcessedActivity, mockContent);
+    expect(content).toBe(content)
   });
 });
