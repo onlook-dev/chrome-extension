0d6bb1475b940329afb62540eb1897bf58123fb7
Kiet Ho <hoakiet98@gmail.com>
2024-04-07 18:33:56 -0400

    Improve dragging

--- editor/src/lib/components/editor/EditorPanel.svelte
+++ editor/src/lib/components/editor/EditorPanel.svelte
@@ -46,7 +46,7 @@
       left: 0,
     },
     disabled: isInputFocused,
-    handle: ".handle",
+    cancel: ".cancel",
   }}
   on:focusin={() => (isInputFocused = true)}
   on:focusout={() => (isInputFocused = false)}
@@ -66,7 +66,7 @@
     <Card.Root class="backdrop-blur bg-background/90 pt-2">
       <Card.Content>
         <Tabs.Root bind:value={selectedTab} class="w-full h-full">
-          <Tabs.List class="handle bg-transparent p-0 gap-4 w-full">
+          <Tabs.List class="bg-transparent p-0 gap-4 w-full">
             <Tabs.Trigger
               class="bg-transparent p-0 text-xs"
               value={TabValue.CSS}>Appearance</Tabs.Trigger
@@ -81,16 +81,16 @@
               >
                 <LineHeight class="w-4 h-4" />
               </button>
-              <button
+              <div
                 class="w-8 h-8 flex items-center justify-center cursor-pointer transition hover:text-white/80"
               >
-                <DragHandleDots2 class="w-4 h-4" />
-              </button>
+                <DragHandleDots2 class="w-4 h-4 pointer-events-none	" />
+              </div>
             </div>
           </Tabs.List>
           <Separator class="mt-1" />
           <div
-            class="h-[calc({cardHeight}-4rem)] overscroll-contain overflow-auto"
+            class="cancel h-[calc({cardHeight}-4rem)] overscroll-contain overflow-auto"
           >
             <Tabs.Content value={TabValue.CSS}
               ><CssTab {editTool} /></Tabs.Content
--- editor/src/lib/components/layers/LayersPanel.svelte
+++ editor/src/lib/components/layers/LayersPanel.svelte
@@ -59,7 +59,7 @@
       top: 0,
       left: 0,
     },
-    handle: ".handle",
+    cancel: ".cancel",
     disabled: isInputFocused,
   }}
   on:focusin={() => (isInputFocused = true)}
@@ -78,7 +78,7 @@
     <Card.Root class="backdrop-blur bg-background/90 pt-2">
       <Card.Content>
         <Tabs.Root bind:value={selectedTab} class="w-full h-full">
-          <Tabs.List class="handle bg-transparent p-0 gap-4 w-full">
+          <Tabs.List class="bg-transparent p-0 gap-4 w-full">
             <Tabs.Trigger
               class="bg-transparent p-0 text-xs"
               value={TabValue.LAYERS}>Layers</Tabs.Trigger
@@ -102,16 +102,16 @@
               >
                 <LineHeight class="w-4 h-4" />
               </button>
-              <button
+              <div
                 class="w-8 h-8 flex items-center justify-center cursor-pointer transition hover:text-white/80"
               >
-                <DragHandleDots2 class="w-4 h-4" />
-              </button>
+                <DragHandleDots2 class="w-4 h-4 pointer-events-none" />
+              </div>
             </div>
           </Tabs.List>
           <Separator class="mt-1" />
           <div
-            class="h-[calc({cardHeight}-4rem)] overscroll-contain overflow-auto"
+            class="cancel h-[calc({cardHeight}-4rem)] overscroll-contain overflow-auto"
           >
             <Tabs.Content value={TabValue.LAYERS}
               ><LayersTab {editTool} />
