0fd743a38f42ac202ee4e31ab0a586302eb5d4bb
Kiet Ho <hoakiet98@gmail.com>
2024-04-28 15:52:32 -0400

    Cleanup

--- extension/tailwind.config.js
+++ extension/tailwind.config.js
@@ -3,35 +3,5 @@ export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
 		extend: {}
-	},
-	plugins: [require('daisyui')],
-	daisyui: {
-		themes: [
-			{
-				light: {
-					...require('daisyui/src/theming/themes')['light'],
-					primary: '#111827',
-					'primary-content': '#ffffff',
-					neutral: '#ebeef5',
-					'neutral-content': '#303133',
-					accent: '#a8abb2',
-					'accent-content': '#ffffff',
-					secondary: '#303133',
-					'secondary-content': '#ffffff',
-					info: '#909399',
-					'info-content': '#ffffff',
-					success: '#67c23a',
-					'success-content': '#ffffff',
-					warning: '#e6a23c',
-					'warning-content': '#ffffff',
-					error: '#f56c6c',
-					'error-content': '#ffffff',
-					'base-100': '#ffffff',
-					'base-200': '#f9fafb',
-					'base-300': '#dfe4e8',
-					'base-content': '#1f2937'
-				}
-			}
-		]
 	}
 }
