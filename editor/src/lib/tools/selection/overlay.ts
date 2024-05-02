fab846f4b1d65417f9f5841926be23869d133018
Kiet Ho <hoakiet98@gmail.com>
2024-05-02 11:49:17 -0400

    Fix top layer issue

--- editor/src/lib/tools/selection/overlay.ts
+++ editor/src/lib/tools/selection/overlay.ts
@@ -9,15 +9,16 @@ interface Rect {
     render: (rect: { width: number, height: number, top: number, left: number }) => void;
 }
 
-class RectImpl implements Rect {
+class RectImpl extends HTMLElement implements Rect {
     element: HTMLElement;
-    svgNamespace: string;
+    svgNamespace: string = 'http://www.w3.org/2000/svg'
     svgElement: Element;
     rectElement: Element;
 
     constructor() {
+        super()
+
         this.element = document.createElement('div')
-        this.svgNamespace = 'http://www.w3.org/2000/svg'
         this.svgElement = document.createElementNS(this.svgNamespace, 'svg')
         this.svgElement.setAttribute('overflow', 'visible')
         this.rectElement = document.createElementNS(this.svgNamespace, 'rect')
@@ -27,12 +28,21 @@ class RectImpl implements Rect {
         this.rectElement.setAttribute('stroke-linecap', 'round')
         this.rectElement.setAttribute('stroke-linejoin', 'round')
         this.svgElement.appendChild(this.rectElement)
-        this.element.style.position = 'absolute'
+        this.element.style.position = 'fixed'
         this.element.style.pointerEvents = 'none' // Ensure it doesn't interfere with other interactions
         this.element.style.zIndex = '999'
         this.element.setAttribute(DATA_ONLOOK_IGNORE, 'true');
         this.element.setAttribute('id', ONLOOK_RECT_ID)
         this.element.appendChild(this.svgElement)
+
+        // Popover fixes
+        this.style.backgroundColor = 'transparent'
+        this.style.border = 'none'
+        this.style.overflow = 'visible'
+        this.style.inset = '0 auto auto 0'
+
+        this.appendChild(this.element)
+        this.setAttribute('popover', 'manual')
     }
 
     render({ width, height, top, left }) {
@@ -43,6 +53,18 @@ class RectImpl implements Rect {
         this.rectElement.setAttribute('height', height)
         this.element.style.top = `${top + window.scrollY}px`
         this.element.style.left = `${left + window.scrollX}px`
+
+        this.hidePopover && this.hidePopover()
+        this.showPopover && this.showPopover()
+    }
+
+    connectedCallback() {
+        this.setAttribute('popover', 'manual')
+        this.showPopover && this.showPopover()
+    }
+
+    disconnectedCallback() {
+        this.hidePopover && this.hidePopover()
     }
 }
 
@@ -261,11 +283,12 @@ export class OverlayManager {
 
     constructor() {
         this.hoverRect = new HoverRect();
+
         this.clickedRects = [];
         this.parentRect = new ParentRect();
 
-        document.body.appendChild(this.hoverRect.element)
-        document.body.appendChild(this.parentRect.element)
+        document.body.appendChild(this.hoverRect)
+        document.body.appendChild(this.parentRect)
     }
 
     clear = () => {
@@ -277,12 +300,14 @@ export class OverlayManager {
     addClickRect = (el: HTMLElement) => {
         if (!el) return
         const clickRect = new ClickRect()
+        document.body.appendChild(clickRect)
         this.clickedRects.push(clickRect)
+
         const rect = el.getBoundingClientRect()
         const margin = window.getComputedStyle(el).margin
         const padding = window.getComputedStyle(el).padding
         clickRect.render({ width: rect.width, height: rect.height, top: rect.top, left: rect.left, padding, margin });
-        document.body.appendChild(clickRect.element)
+
     }
 
     updateParentRect = (el: HTMLElement) => {
@@ -313,3 +338,8 @@ export class OverlayManager {
         this.parentRect.render({ width: 0, height: 0, top: 0, left: 0 })
     }
 }
+
+customElements.define('rect-impl', RectImpl);
+customElements.define('hover-rect', HoverRect);
+customElements.define('click-rect', ClickRect);
+customElements.define('parent-rect', ParentRect);
