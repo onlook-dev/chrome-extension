c499659393ba334cefdb442a186338211a4bc199
Kiet Ho <hoakiet98@gmail.com>
2024-05-06 18:08:31 +0000

    Update welcome page pt.3

--- dashboard/src/routes/welcome/+page.svelte
+++ dashboard/src/routes/welcome/+page.svelte
@@ -8,43 +8,44 @@
 	style="background-image: url({BackgroundImage}); background-size: cover; background-position: center;"
 >
 	<div class="flex flex-col w-2/3 items-center gap-6">
-		<h1 class="text-2xl text-white text-start w-full">Let's push some pix</h1>
+		<h1 class="text-2xl text-white text-start w-full">Let's push some pixels</h1>
 		<div class="flex flex-row w-full gap-2.5 h-auto bg-transparent p-0 h-auto">
 			<div class="w-1/2 h-full bg-black rounded-none px-4">
-				<div class="text-light">
+			
+				<div class="text-gray-600">Step 1</div>
 					<h1 class="text-white">Onlook</h1>
 					<h2 class="text-tertiary">Where Creativity meets Code</h2>
 				</div>
 				<div class="space-y-4">
-					<div class="w-full h-[68px] bg-stone-100 mb-5"></div>
+					<div class="w-full h-[150px] bg-[#3b3b3b] mt-5 mb-5"></div>
 				</div>
-				<div class="flex flex-col text-white">
+				<div class="flex flex-col text-gray-400 text-sm">Click extensions in the upper right corner</div>
 					<p class="text-white">Hello</p>
-					<p>World</p>
+					<p class="text-gray-400 text-sm">Pin Onlook</p>
 				</div>
 			</div>
-
+<div class="p-5 border"></div>
 			<div class="w-1/2 rounded-none px-4 bg-black h-full p-2">
-				<div class="text-white">
+				<div class="text-gray-600">Step 2</div>
 					<h1 class="">Onlook</h1>
 					<h2 class="text-tertiary">Where Creativity meets Code</h2>
 				</div>
 				<div class="space-y-4">
-					<div class="w-full h-[68px] bg-stone-100 mb-5"></div>
+					<div class="w-full h-[150px] bg-[#3b3b3b] mt-5 mb-5"></div>
 				</div>
-				<div class="flex flex-col text-white">
-					<p class="text-white">Hello</p>
+				<div class="flex flex-col text-gray-400 text-sm">Go to any page you want to edit</div>
+					<p class="text-white text-sm text-[#a3a3a3]">Hello Click the extension and start editing</p>
 					<p>World</p>
 				</div>
 			</div>
 		</div>
-
+<div class="border">
 		<div class="w-full h-[fit-content] rounded-none px-4 bg-black p-5">
-			<div class="text-white">
-				<h1 class="text-2xl">Onlook</h1>
+			<div class="text-white text-xl">See Onlook in action</div>
+				<h1 class="text-2xl text-[#a3a3a3]">Watch us walk through a Demo project and make a pull request in less than 1 minute</h1>
 				<h2 class="text-xl text-tertiary">Where Creativity meets Code</h2>
 			</div>
-			<div class="space-y-4">
+			<div class="space-y-4 h-[150px] mt-5 bg-gray-800">
 				<div class="w-full h-[88px] bg-stone-100 my-5"></div>
 			</div>
 			<div>
