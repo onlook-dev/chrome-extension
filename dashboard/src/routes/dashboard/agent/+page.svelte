a4c370ffae6e59cbe3015bf0279cc894b80b88f7
Kiet Ho <hoakiet98@gmail.com>
2024-05-06 09:19:25 -0400

    Fix fault

--- dashboard/src/routes/dashboard/agent/+page.svelte
+++ dashboard/src/routes/dashboard/agent/+page.svelte
@@ -97,7 +97,7 @@
 	}
 </script>
 
-<div class="flex flex-col w-screen h-screen items-center mt-20 justify-center p-5 m-0 h-screen mt-5 w-full"></div>
+<div class="flex flex-col w-screen h-screen items-center justify-center p-5 m-0">
 	<form class="flex flex-row space-x-2 w-96">
 		<Input
 			on:keypress={(e) => {
@@ -117,12 +117,16 @@
 		<div id="test" class="w-10 h-10 bg-stone-500"></div>
 	</div>
 
-	<div class="mt-10 w-96 h-full flex flex-col overflow-auto space-y-2 p-2.5 rounded border border-gray-300">
+	<div
+		class="mt-10 w-96 h-full flex flex-col overflow-auto space-y-2 p-2.5 rounded border border-gray-300"
+	>
 		{#each logs as log}
 			{#if log.author === Author.User}
 				<div class="bg-blue-500 text-white p-2 rounded ml-auto">{log.message}</div>
 			{:else}
-				<div class="bg-green-500 text-white p-2 rounded mr-auto w-[50%] h-[fit-content]">{log.message}</div>
+				<div class="bg-green-500 text-white p-2 rounded mr-auto w-[50%] h-[fit-content]">
+					{log.message}
+				</div>
 			{/if}
 		{/each}
 	</div>
