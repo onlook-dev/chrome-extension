e01ba30e23755a0e5180b2d24eba45b3c7cdaa12
Kiet Ho <hoakiet98@gmail.com>
2024-06-01 11:13:08 -0400

    Add JSON into data-onlook-id

--- plugins/svelte/index.js
+++ plugins/svelte/index.js
@@ -4,12 +4,12 @@ import fs from "fs";
 
 import { parse, walk } from "svelte/compiler";
 import { DATA_ONLOOK_ID } from "../shared/constants.js";
-import { generateDataAttributeValue, getCurrentCommit } from "../shared/helpers.js";
+import { getCurrentCommit } from "../shared/helpers.js";
+import { strToU8, compressSync, decompressSync } from 'fflate'
 
 export const onlookPreprocess = ({ root = path.resolve('.'), absolute = false, commit_hash = getCurrentCommit() }) => {
   return {
     markup: ({ content, filename }) => {
-      let snapshotAdded = false;
       const nodeModulesPath = path.resolve(root, "node_modules");
       const sveltekitPath = path.resolve(root, ".svelte-kit");
 
@@ -37,39 +37,11 @@ export const onlookPreprocess = ({ root = path.resolve('.'), absolute = false, c
         enter(node) {
           if (node.type === "Element") {
             // Calculate the line number for each element node
-            const lineStart =
-              content.slice(0, node.start).split("\n").length + offset;
+            const attributeValue = getDataOnlookId(node, content, offset, filename, commit_hash, root, absolute);
 
-            const lineClosing = content.slice(0, node.end).split("\n").length + offset;
-
-            // Find the end of the opening tag
-            const tagContent = content.slice(node.start, node.end);
-            let endOfOpeningTag = findEndOfOpeningTag(
-              tagContent,
-              node.start,
-              node.selfClosing
-            );
-            const lineEnd =
-              content.slice(0, endOfOpeningTag).split("\n").length + offset;
-
-            // Find the position to insert the attribute
-            const startTagEnd = node.start + node.name.length + 1;
-            const attributeValue = generateDataAttributeValue(
-              filename,
-              lineStart,
-              lineEnd,
-              lineClosing,
-              root,
-              absolute
-            );
             const attributeName = `${DATA_ONLOOK_ID}='${attributeValue}'`;
+            const startTagEnd = node.start + node.name.length + 1;
             s.appendLeft(startTagEnd, ` ${attributeName}`);
-
-            if (!snapshotAdded && node.name === "div" && commit_hash) {
-              const hiddenInput = `<input type="hidden" data-onlook-snapshot="${commit_hash}" />`;
-              s.append(`\n${hiddenInput}`);
-              snapshotAdded = true;
-            }
           }
         },
       });
@@ -116,4 +88,41 @@ function findEndOfOpeningTag(tagContent, start, selfClosing) {
     }
   }
   return index;
+}
+
+function getDataOnlookId(node, content, offset, filename, commit, root, absolute) {
+  const lineStart =
+    content.slice(0, node.start).split("\n").length + offset;
+
+  const lineClosing = content.slice(0, node.end).split("\n").length + offset;
+
+  // Find the end of the opening tag
+  const tagContent = content.slice(node.start, node.end);
+  let endOfOpeningTag = findEndOfOpeningTag(
+    tagContent,
+    node.start,
+    node.selfClosing
+  );
+  const lineEnd =
+    content.slice(0, endOfOpeningTag).split("\n").length + offset;
+
+  // Find the position to insert the attribute
+  const startTagEnd = node.start + node.name.length + 1;
+
+  const domNode = {
+    path: absolute ? filename : path.relative(root, filename),
+    startTag: { start: { line: lineStart, column: startTagEnd }, end: { line: lineStart, column: endOfOpeningTag } },
+    endTag: { start: { line: lineClosing, column: node.end }, end: { line: lineClosing, column: node.end } },
+    commit
+  };
+
+  // Compress
+  const buf = strToU8(JSON.stringify(domNode));
+  const compressed = compressSync(buf);
+  const base64 = Buffer.from(compressed).toString('base64');
+
+  // Decompress
+  console.log(strFromU8(decompressSync(Buffer.from(base64, 'base64'))));
+
+  return base64;
 }
