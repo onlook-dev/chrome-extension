b20b6f6370b8046d3ddbeb2919babee2e1825b92
Kiet Ho <hoakiet98@gmail.com>
2024-06-03 21:15:59 +0000

    Testing in production with compiler

--- dashboard/src/routes/welcome/+page.svelte
+++ dashboard/src/routes/welcome/+page.svelte
@@ -9,17 +9,17 @@
 	class="flex flex-col items-center justify-center w-screen h-screen bg-cover bg-center"
 	style="background-image: url({BackgroundImage});"
 >
-	<div class="flex flex-col w-1/2 items-center gap-6 min-w-[800px] gap-[20px]">
-		<h1 class="text-2xl text-white text-start w-full">Let's push some pixels</h1>
+	<div class="flex flex-col w-1/2 items-center min-w-[800px] gap-[20px]">
+		<h1 class="text-2xl text-white text-start w-full">Let's fuck shit up</h1>
 		<div class="flex flex-row w-full justify-center h-full gap-5 bg-transparent p-0">
-			<div class="w-full h-full bg-black rounded-none p-6 border">
+			<div class="w-full h-full bg-black rounded-none p-6 border" style="background-color: #ffffff; border-radius: 25px;">
 				<div class="text-light">
 					<h1 class="text-tertiary">Step 1</h1>
-					<h2 class="text-tertiary text-white">Pin Onlook in your browser</h2>
+					<h2 class="text-tertiary text-white" style="color: #707070;">Pin Onlook in your browser</h2>
 				</div>
 				<img class="object-cover w-full bg-[#363636] my-4" src={Pin} alt="Pin extension tip" />
 				<div class="flex flex-col text-white gap-2">
-					<p class="text-gray-400 text-sm flex flex-row items-center">
+					<p class="text-gray-400 text-sm flex flex-row items-center" style="color: #707070;" style="color: #707070;">
 						<svg
 							class="mr-2"
 							width="15"
@@ -42,7 +42,7 @@
 				</div>
 			</div>
 
-			<div class="w-full rounded-none bg-black h-full border p-6">
+			<div class="w-full rounded-none bg-black h-full border p-6" style="background-color: #ffffff; border-radius: 25px;">
 				<div class="text-white">
 					<h1 class="text-tertiary">Step 2</h1>
 					<h2 class="text-tertiary text-white">Start editing any page</h2>
@@ -52,14 +52,14 @@
 					<p class="text-gray-400 text-sm flex flex-row items-center">
 						<Link1 class="text-primary w-4 h-4 mr-2" /> Go to any page you want to edit
 					</p>
-					<p class="text-tertiary text-sm flex flex-row items-center">
+					<p class="text-tertiary text-sm flex flex-row items-center" style="color: #707070;">
 						<Pencil2 class="text-primary w-4 h-4 mr-2" />Click the extension and start editing
 					</p>
 				</div>
 			</div>
 		</div>
 
-		<div class="flex flex-row w-full h-[fit-content] rounded-none bg-black p-6 border gap-[40px]">
+		<div class="flex flex-row w-full h-[fit-content] rounded-none bg-black p-6 border gap-[40px]" style="border-radius: 25px; background-color: #ffffff;">
 			<iframe
 				class="w-3/5 h-[150px]"
 				src="https://www.youtube.com/embed/pUzCOpIE1zQ?si=cnwzWINbIT88ZiAU"
@@ -70,13 +70,20 @@
 				allowfullscreen
 			></iframe>
 			<div class="text-white flex flex-col items-start m-0 p-5 gap-2">
-				<p class="text-white">See Onlook in action</p>
+				<p class="text-white" style="color: #707070">See Onlook in action</p>
 				<p class="text-sm text-tertiary">
 					Watch us walk through a Demo project and make a pull request in less than 1 minute
 				</p>
-				<a class="text-sm text-red-600 hover:underline flex flex-row items-center text-[#ef4444]" href="https://onlook.dev/blog/create-your-first-pull-request" target="_blank">Read our guide to getting started <ExternalLink class="w-4 h-4 ml-2" /></a
+				<a
+					class="text-sm hover:underline flex flex-row items-center text-[#ef4444]"
+					href="https://onlook.dev/blog/create-your-first-pull-request"
+					target="_blank">Read our guide to getting started <ExternalLink class="w-4 h-4 ml-2" /></a
 				>
-				<a class="text-sm text-brand text-[#ef4444] hover:underline flex flex-row items-center" href="https://onlook.dev/blog/installing-onlook" target="_blank">Set up Onlook in your own Github Repo <ExternalLink class="w-4 h-4 ml-2" />
+				<a
+					class="text-sm text-brand text-[#ef4444] hover:underline flex flex-row items-center"
+					href="https://onlook.dev/blog/installing-onlook"
+					target="_blank"
+					>Set up Onlook in your own Github Repo <ExternalLink class="w-4 h-4 ml-2" />
 				</a>
 			</div>
 		</div>
@@ -88,7 +95,11 @@
 			<div class="text-tertiary text-[14px]">Let's get you set up</div>
 		</div>
 
-		<a class="flex flex-row items-center rounded-xs bg-red-500/20 py-2 px-3 text-red-500 w-fit hover:bg-red-500/30 ml-[18px]" href="https://calendar.app.google/hjCAg7HpczY39Zd4A" target="_blank">
+		<a
+			class="flex flex-row items-center rounded-xs bg-red-500/20 py-2 px-3 text-red-500 w-fit hover:bg-red-500/30 ml-[18px]"
+			href="https://calendar.app.google/hjCAg7HpczY39Zd4A"
+			target="_blank"
+		>
 			<Calendar class="w-4 h-4 mr-2" />
 			<div class="text-sm">Chat with a founder</div>
 		</a>
