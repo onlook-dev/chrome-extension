d3a3fcff6e76ebefb652417313cf9f7742f9339d
Kiet Ho <hoakiet98@gmail.com>
2024-06-04 14:56:30 -0400

    Add prompting service

--- extension/src/lib/translation/index.ts
+++ extension/src/lib/translation/index.ts
@@ -4,20 +4,15 @@ import { JsonOutputKeyToolsParser } from "@langchain/core/output_parsers/openai_
 import { Runnable } from "@langchain/core/runnables";
 import { InlineCssPromptService, TailwindPromptService, TextPromptService } from "./prompt";
 import { langfuseConfig, openAiConfig } from "$lib/utils/env";
-import { StyleFramework } from "$shared/models";
 import { CallbackHandler } from "langfuse-langchain";
 
 export class TranslationService {
   private openAi: Runnable;
-  private inlineCssPromptService: InlineCssPromptService;
-  private tailwindPromptService: TailwindPromptService;
   private textPromptService: TextPromptService;
   private traceHandler: CallbackHandler;
 
   constructor(private projectId: string = "default") {
     this.openAi = this.getModel();
-    this.inlineCssPromptService = new InlineCssPromptService();
-    this.tailwindPromptService = new TailwindPromptService();
     this.textPromptService = new TextPromptService();
     this.traceHandler = new CallbackHandler({ ...langfuseConfig, sessionId: this.projectId });
   }
@@ -29,28 +24,11 @@ export class TranslationService {
       temperature: 0,
       cache: true,
     }).bind({
-      tools: [translationTool],
-      tool_choice: translationTool
+      tools: [translationTool]
     }).pipe(new JsonOutputKeyToolsParser({
       keyName: translationTool.function.name,
       returnSingle: true,
     }));
-  }
-
-  async getStyleTranslation(variables: typeof this.inlineCssPromptService.inputVariables, styleFramework?: StyleFramework): Promise<string> {
-    let prompt;
-    switch (styleFramework) {
-      case StyleFramework.TailwindCSS:
-        prompt = await this.tailwindPromptService.getPrompt(variables);
-        break;
-      case StyleFramework.InlineCSS:
-        prompt = await this.inlineCssPromptService.getPrompt(variables);
-        break;
-      default:
-        prompt = await this.inlineCssPromptService.getPrompt(variables);
-    }
-    const response = (await this.openAi.invoke(prompt, { callbacks: [this.traceHandler], runName: "Style run" })) as { code: string }
-    return response.code.trim();
   }
 
   async getTextTranslation(variables: typeof this.textPromptService.inputVariables): Promise<string> {
