5a8b96e57aba6c3ba97610bf5f4727f93ff69696
Kiet Ho <hoakiet98@gmail.com>
2024-06-03 22:42:02 -0400

    New prompt tab

---
+++ editor/src/lib/components/editor/ChatTab.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import type { EditTool } from "$lib/tools/edit";
+    import { Input } from "$lib/components/ui/input/index.js";
+    import { Separator } from "$lib/components/ui/separator/index.js";
+
+    export let editTool: EditTool;
+    export let cardHeight: string;
+</script>
+
+<div class="w-full h-full">
+    <div class="w-full h-[calc({cardHeight}-170px)]"></div>
+    <Separator />
+    <div class="mt-4 p-[2px]">
+        <Input class="text-xs" type="text" placeholder="Your style change" />
+    </div>
+</div>
--- editor/src/lib/components/editor/EditorPanel.svelte
+++ editor/src/lib/components/editor/EditorPanel.svelte
@@ -4,25 +4,26 @@
   import { editorPanelVisible } from "$lib/states/editor";
   import { DragHandleDots2, Minus, Size } from "radix-icons-svelte";
   import { draggable } from "$lib/utils";
+  import { ToolName, type ToolManager } from "$lib/tools";
 
   import * as Card from "$lib/components/ui/card";
   import * as Tabs from "$lib/components/ui/tabs";
   import CssTab from "./CssTab.svelte";
   import Button from "../ui/button/button.svelte";
+  import ChatTab from "./ChatTab.svelte";
 
   import type { EditTool } from "$lib/tools/edit";
-  import { ToolName, type ToolManager } from "$lib/tools";
 
   enum TabValue {
-    CSS = "css",
-    CODE = "code",
+    MANUAL = "manual",
+    ASSISTED = "assisted",
   }
 
   export let toolManager: ToolManager;
   let editTool: EditTool = toolManager.editTool;
 
   const cardWidth = "232px";
-  let selectedTab: string = TabValue.CSS;
+  let selectedTab: string = TabValue.ASSISTED;
   let isInputFocused = false;
   let panelCollapsed = false;
   let cardRef: HTMLDivElement;
@@ -62,41 +63,51 @@
   >
     <Card.Root class="backdrop-blur h-full bg-background/90 pt-2">
       <Card.Content>
-        <Tabs.Root bind:value={selectedTab} class="w-full h-full">
-          <Tabs.List
-            data-drag-handle
-            class="bg-transparent p-0 gap-4 w-full select-none"
+        <div class="flex flex-row mt-2" data-drag-handle>
+          <Button
+            class="h-6 rounded-sm text-xs transition"
+            variant="primary"
+            on:click={() => toolManager.selectTool(ToolName.PUBLISH)}
+            >Finish Designing</Button
           >
-            <Button
-              class="h-6 rounded-sm text-xs transition"
-              variant="primary"
-              on:click={() => toolManager.selectTool(ToolName.PUBLISH)}
-              >Finish Designing</Button
+          <div class="ml-auto flex items-center">
+            <div
+              class="w-8 h-8 flex items-center justify-center cursor-pointer transition hover:text-white/80"
             >
-            <div class="ml-auto flex items-center">
-              <div
-                class="w-8 h-8 flex items-center justify-center cursor-pointer transition hover:text-white/80"
-              >
-                <DragHandleDots2 class="w-4 h-4 pointer-events-none	" />
-              </div>
-              <button
-                class="w-8 h-8 flex items-center justify-center transition hover:text-white/80"
-                on:click={collapsePanel}
-              >
-                {#if panelCollapsed}
-                  <Size class="w-4 h-4" />
-                {:else}
-                  <Minus class="w-4 h-4" />
-                {/if}
-              </button>
+              <DragHandleDots2 class="w-4 h-4 pointer-events-none	" />
             </div>
+            <button
+              class="w-8 h-8 flex items-center justify-center transition hover:text-white/80"
+              on:click={collapsePanel}
+            >
+              {#if panelCollapsed}
+                <Size class="w-4 h-4" />
+              {:else}
+                <Minus class="w-4 h-4" />
+              {/if}
+            </button>
+          </div>
+        </div>
+        <Tabs.Root bind:value={selectedTab} class="w-full h-full">
+          <Tabs.List class="bg-transparent p-0 gap-4 select-none">
+            <Tabs.Trigger
+              class="bg-transparent p-0 text-xs"
+              value={TabValue.MANUAL}>Manual styles</Tabs.Trigger
+            >
+            <Tabs.Trigger
+              class="bg-transparent p-0 text-xs"
+              value={TabValue.ASSISTED}>Assisted styles</Tabs.Trigger
+            >
           </Tabs.List>
           <Separator class="mt-1" />
           <div
             class="h-[calc({cardHeight}-4rem)] overscroll-contain overflow-auto"
           >
-            <Tabs.Content value={TabValue.CSS}
+            <Tabs.Content value={TabValue.MANUAL}
               ><CssTab {editTool} /></Tabs.Content
+            >
+            <Tabs.Content value={TabValue.ASSISTED}
+              ><ChatTab {editTool} {cardHeight} /></Tabs.Content
             >
           </div>
           <Card.Footer class="flex justify-between"></Card.Footer>
