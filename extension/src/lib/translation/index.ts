e9cc12bfa58fa0dd0d53a2aff23e4779b8486ca7
Kiet Ho <hoakiet98@gmail.com>
2024-06-10 14:15:40 -0400

    Working with structured output langchain 0.2

--- extension/src/lib/translation/index.ts
+++ extension/src/lib/translation/index.ts
@@ -1,40 +1,57 @@
 import { ChatOpenAI } from "@langchain/openai";
-import { styleTool } from "./tools";
-import { JsonOutputKeyToolsParser } from "@langchain/core/output_parsers/openai_tools";
-import { Runnable } from "@langchain/core/runnables";
 import { StylePromptService } from "./prompt";
 import { langfuseConfig, openAiConfig } from "$lib/utils/env";
 import { CallbackHandler } from "langfuse-langchain";
+import { type RunnableConfig } from "@langchain/core/runnables";
+import { ChatPromptTemplate } from "@langchain/core/prompts";
+import { createOpenAIFunctionsAgent, AgentExecutor } from "langchain/agents";
+import { type StructuredToolInterface } from "@langchain/core/tools";
+import { z } from "zod";
+import { DynamicStructuredTool } from "@langchain/community/tools/dynamic";
 
 export class TranslationService {
-  private openAi: Runnable;
   private stylePromptService: StylePromptService;
   private traceHandler: CallbackHandler;
 
+  private styleResponse = z.object({
+    changes: z.array(z.object({
+      property: z.string().describe("The CSS property to change. Must be a valid CSS property."),
+      value: z.string().describe("The value to set the property to. Must be a valid CSS value. Hex code must have hastag # in front."),
+    })).describe("An array of style changes to make to the style"),
+    summary: z.string().describe("A summary of the changes made"),
+  })
+
   constructor(private projectId: string = "default") {
-    this.openAi = this.getModel();
     this.stylePromptService = new StylePromptService();
     this.traceHandler = new CallbackHandler({ ...langfuseConfig, sessionId: this.projectId });
   }
 
-  private getModel() {
-    return new ChatOpenAI({
+  async getStyleChange(variables: typeof this.stylePromptService.inputVariables): Promise<z.infer<typeof this.styleResponse> | { error: string }> {
+    const config: RunnableConfig = {
+      configurable: { sessionId: "1" },
+      callbacks: [this.traceHandler],
+      runName: "Style run"
+    };
+
+    const model = new ChatOpenAI({
       openAIApiKey: openAiConfig.apiKey,
       modelName: "gpt-4o",
       temperature: 0,
       cache: true,
-    }).bind({
-      tools: [styleTool],
-      tool_choice: styleTool
-    }).pipe(new JsonOutputKeyToolsParser({
-      keyName: styleTool.function.name,
-      returnSingle: true,
-    }));
-  }
+    }).withStructuredOutput(this.styleResponse)
 
-  async getStyleChange(variables: typeof this.stylePromptService.inputVariables): Promise<string> {
+    // Pass message history here
     const prompt = await this.stylePromptService.getPrompt(variables);
-    const response = await this.openAi.invoke(prompt, { callbacks: [this.traceHandler], runName: "Style run" });
-    return response;
+    try {
+      console.log("Prompt", prompt);
+      const response = await model.invoke(prompt, config);
+      console.log("Response", response);
+      return response;
+    } catch (error) {
+      console.error(error);
+      return {
+        error: `${error}`
+      }
+    }
   }
 }
