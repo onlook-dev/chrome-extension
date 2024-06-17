fbc2c9c7591cdbcf77d163dd7163a6314077f709
Kiet Ho <hoakiet98@gmail.com>
2024-06-17 15:27:41 -0400

    Remove build error

--- docs/src/app/quickstart/page.tsx
+++ docs/src/app/quickstart/page.tsx
@@ -211,23 +211,3 @@ export default function DocumentationPage() {
     </div>
   )
 }
-
-function MountainIcon(props) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="m8 3 4 8 5-5 5 15H2L8 3z" />
-    </svg>
-  )
-}
-
