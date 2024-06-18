e113567de2cb0e9569f8473f91b94a90ce32686e
Kiet Ho <hoakiet98@gmail.com>
2024-06-17 20:37:02 -0400

    Make docs blank

--- docs/src/app/layout.tsx
+++ docs/src/app/layout.tsx
@@ -1,11 +1,14 @@
 import "@/styles/globals.css"
-
-import type { Metadata, Viewport } from "next"
+import Link from "next/link"
+import Logo from "@/components/logo"
+import { Input } from "@/components/ui/input"
+import { Search } from "lucide-react"
+import { ModeToggle } from "@/components/mode-toggle"
 import { Inter } from "next/font/google"
-
 import { siteConfig } from "@/config/site"
 import { cn } from "@/lib/utils"
 import { ThemeProvider } from "@/components/theme-provider"
+import type { Metadata, Viewport } from "next"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -67,7 +70,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en" suppressHydrationWarning>
       <head >
-        <link rel="icon" href="/logo.png" sizes="any" />
+        <link rel="icon" href="/logo.ico" sizes="any" />
       </head>
       <body
         className={cn(
@@ -80,7 +83,32 @@ export default function RootLayout({ children }: RootLayoutProps) {
           defaultTheme="system"
           enableSystem
           disableTransitionOnChange
-        >
+        >   <header className="sticky top-0 z-50 flex w-full items-center border-b bg-white px-6 shadow-sm dark:border-gray-800 dark:bg-gray-950">
+            <Link className="flex items-center gap-4" href="#">
+              <Logo width={25} height={25} />
+              <span className="text-lg font-semibold">Onlook Docs</span>
+            </Link>
+            <nav className="h-12 ml-auto flex items-center gap-4">
+              <ModeToggle />
+              <Link className="text-sm font-medium hover:underline underline-offset-4" href="#">
+                Docs
+              </Link>
+              <Link className="text-sm font-medium hover:underline underline-offset-4" href="#">
+                Blog
+              </Link>
+              <Link className="text-sm font-medium hover:underline underline-offset-4" href="#">
+                Community
+              </Link>
+              <div className="relative">
+                <Input
+                  className="h-8 w-48 rounded-md border border-gray-200 bg-gray-100 px-3 text-sm focus:border-gray-300 focus:outline-none dark:border-gray-800 dark:bg-gray-900 dark:text-gray-50"
+                  placeholder="Search docs..."
+                  type="search"
+                />
+                <Search className="absolute right-2 top-1/2 h-4 w-4 -translate-y-1/2 text-gray-400 dark:text-gray-500" />
+              </div>
+            </nav>
+          </header>
           {children}
         </ThemeProvider>
       </body>
