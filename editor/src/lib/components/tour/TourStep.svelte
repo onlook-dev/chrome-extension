1f10188e05ff0706345b86003fc527abdb9d2b4f
Kiet Ho <hoakiet98@gmail.com>
2024-06-19 18:05:30 -0400

    Cleanup

--- editor/src/lib/components/tour/TourStep.svelte
+++ editor/src/lib/components/tour/TourStep.svelte
@@ -1,11 +1,12 @@
-<script>
+<script lang="ts">
     import { Button } from "$lib/components/ui/button";
     import * as Card from "$lib/components/ui/card";
-    export let classes = "";
 
+    export let classes = "";
     export let stage;
     export let buttonText = "Next";
     export let headerText = "How to use Onlook";
+    export let callback: (() => void) | undefined = undefined;
 </script>
 
 <Card.Root class="w-[20rem] bg-blue-500/95 border-blue-400 m-auto {classes}">
@@ -17,6 +18,11 @@
         <Button class="ml-auto" variant="ghost" on:click={() => (stage -= 1)}>
             Back
         </Button>
-        <Button on:click={() => (stage += 1)}>{buttonText}</Button>
+        <Button
+            on:click={() => {
+                stage += 1;
+                callback && callback();
+            }}>{buttonText}</Button
+        >
     </Card.Footer>
 </Card.Root>
